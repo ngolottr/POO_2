30f0e39a94414d524ef54b0f2366080aaa9cfcc9
ngolottr <contacto.nicolaspk@gmail.com>
2017-11-02 13:57:18 -0400

    Se añadio una nueva clase con sus constructores y sus getter y setter, ademas se añadio un nuevo print en la main con un hola mundo 2

--- Proyecto_Prueba/src/main/Main.java
+++ Proyecto_Prueba/src/main/Main.java
@@ -3,12 +3,13 @@ package main;
 
 /*
 autor: Nicolas Golott
-Fecha: 02 / 11 / 2017
+Fecha: 02 / 11 / 2017   
 */
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Hola mundo 2");
     }
     
 }
