d58d6f7817f85a8b90f07f175c273acb1bea9293
ngolottr <contacto.nicolaspk@gmail.com>
2017-11-02 13:40:54 -0400

    Se creó la clase Main con un Hola mundo, ademas se añadieron comentarios de autor y fecha

---
+++ Proyecto_Prueba/src/main/Main.java
@@ -0,0 +1,14 @@
+
+package main;
+
+/*
+autor: Nicolas Golott
+Fecha: 02 / 11 / 2017
+*/
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
